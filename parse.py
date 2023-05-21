3a0a24993b1ba3fcace2eb6721b50d820fdbc03b
Sebastian B <sebastian.barzaghi@outlook.com>
2023-05-21 12:09:12 +0200

    feat: script parse.py and citator.py

---
+++ parse.py
@@ -0,0 +1,76 @@
+import os, json, codecs
+from pymarc import marcxml
+
+# Create error log
+error_log = codecs.open('parse-marc-errors.log', 'w', encoding='utf-8')
+
+# Define the list of fields to extract
+fields_to_extract = [
+    ('identifier', '770', 'o'),
+    ('title', '245', 'a'),
+    ('abbreviatedTitle', '210', 'a'),
+    ('keyTitle', '222', 'a'),
+    ('uniformTitle', '240', 'a'),
+    ('author', '100', 'a'),
+    ('placeOfPublication', '260', 'a'),
+    ('publisher', '260', 'b'),
+    ('dateOfPublication', '260', 'c'),
+    ('edition', '250', 'a'),
+    ('extent', '300', 'a'),
+    ('physicalDetails', '300', 'b'),
+    ('dimensions', '300', 'c'),
+    ('publicationFrequency', '310', 'a'),
+    ('contentType', '336', 'a'),
+    ('mediaType', '337', 'a'),
+    ('medium', '340', 'a'),
+    ('accessibilityContent', '341', 'a'),
+    ('series', '490', 'a'),
+    ('note', '500', 'a'),
+    ('preferredCitation', '524', 'a'),
+    ('isbn', '020', 'a'),
+    ('termsOfAvailability', '020', 'c'),
+    ('qualifyingInformation', '020', 'q'),
+    ('issn', '022', 'a'),
+    ('lcControlNumber', '010', 'a'),
+    ('holdingInstitution', '850', 'a'),
+    ('location', '852', 'a')
+]
+
+# Prepare the list of dictionaries
+data = []
+
+# Define the path to the folder containing the MARCXML files
+folder_path = 'marc-files'
+
+# Iterate over the folders in the specified folder
+for folder in os.listdir(folder_path):
+
+    # Construct the path to the MARCXML file in the current folder
+    file_path = os.path.join(folder_path, folder, folder + '_marc.xml')
+
+    # Check if the file exists
+    if os.path.isfile(file_path):
+        # Open the MARCXML file
+        with open(file_path, 'rb') as marc_file:
+            records = marcxml.parse_xml_to_array(marc_file)
+    
+            # Iterate over the MARC records in the file
+            for record in records:
+                value = {}
+                # Get the Title and Author fields from the record
+                for field_name, field_label, subfield in fields_to_extract:
+                    try:
+                        value[field_name] = record[field_label][subfield]
+                    except Exception as e:
+                        error_log.write(f"Could not find {field_name}-{field_label}-{subfield} in {record} because of error: {e}\n")
+                        print(f"Could not find {field_name}-{field_label}-{subfield} in {record} because of error: {e}\n")
+                    else:
+                        print(f"Parsing...")
+
+                # Popoulate the list of dictionaries
+                data.append(value)
+
+# Save the data as a JSON file
+with open("data.json", "w") as outfile:
+    json.dump(data, outfile)
+                
